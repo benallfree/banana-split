2da4b202cf047a26a6e3c10dcd13c118a32de073
Ben Allfree <ben@benallfree.com>
2025-04-16 04:41:40 -0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { pages } from "vike-cloudflare";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import vike from "vike/plugin";
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { pages } from 'vike-cloudflare'
+import vike from 'vike/plugin'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vike(), react({}), tailwindcss(), pages()],
   build: {
-    target: "es2022",
+    target: 'es2022',
   },
-});
+})
